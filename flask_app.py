04baecade6de8b6aaaafe60e7fa77443ffd546d4
tomes2000 <aavash2000@gmail.com>
2023-08-09 21:46:43 +0100

    adding connection options

--- flask_app.py
+++ flask_app.py
@@ -200,3 +200,9 @@ def delete_cuisine(cuisine_id):
     flash("Cuisine Successfully Deleted")
     return redirect(url_for("get_cuisines"))
 
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+
