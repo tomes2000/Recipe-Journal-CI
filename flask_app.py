6d0d8381ae0296457c3ad410e169dd0731d4017b
tomes2000 <aavash2000@gmail.com>
2023-08-09 21:25:11 +0100

    adding mongo configurations

--- flask_app.py
+++ flask_app.py
@@ -10,3 +10,8 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
