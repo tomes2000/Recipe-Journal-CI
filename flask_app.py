574b676bb47013f30eccb0a484ce312c29002b48
tomes2000 <aavash2000@gmail.com>
2023-08-11 19:51:23 +0100

    set debug=False

--- flask_app.py
+++ flask_app.py
@@ -212,5 +212,5 @@ def delete_cuisine(cuisine_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
 
